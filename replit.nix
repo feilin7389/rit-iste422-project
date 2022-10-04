3cf336a163e6b962310bbed927794e499fd8e6d7
Fei Lin <78882445+feilin7389@users.noreply.github.com>
2022-10-04 05:11:06 +0000

    adding the gradle file

---
+++ replit.nix
@@ -0,0 +1,11 @@
+{ pkgs }: {
+    deps = [
+        pkgs.gradle_6
+        pkgs.gradle_6
+        pkgs.gradle_6
+        pkgs.graalvm17-ce
+        pkgs.maven
+        pkgs.replitPackages.jdt-language-server
+        pkgs.replitPackages.java-debug
+    ];
+}
